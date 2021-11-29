9ef00b554c42be07645772b2c77c4edd663506eb
Lucca <mario-lucca@hotmail.com>
2021-11-29 20:59:07 -0300

    Alterando método de alterar perfil por nif (usuário admin) para verificar se existe senha antes de chamar o bcrypt. E por estarmos tratando com select no front-end, criamos uma condição para caso o depto seja 0 ele o valor do departamento será o do próprio usuário que está sendo mudado.

--- src/app/constants/pedido.constant.js
+++ src/app/constants/pedido.constant.js
@@ -1,5 +1,6 @@
 module.exports = {
     notFound: "Pedido não encontrado!",
     notFoundArray: "Nenhum pedido encontrado!",
-    invalidParameter: "Insira um parâmetro válido!"
+    invalidParameter: "Insira um parâmetro válido!",
+    alreadyRated: "Esse pedido já foi avaliado!"
 };
